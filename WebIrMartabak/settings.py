6f2c90d89553de2b65a41158a5253d7b38387c67
Shaddamah <shaddam.a.h@gmail.com>
2020-04-11 12:13:13 +0700

    Set Django Database URL

--- WebIrMartabak/settings.py
+++ WebIrMartabak/settings.py
@@ -1,6 +1,6 @@
+import dj-database-url
 import os
 import django_heroku
-import dj-database-url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
